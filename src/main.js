46859d725bab8d220389619115e0f1033f068fb9
ssudya18-sys <ssudya18@gmail.com>
2026-09-21 20:43:52 +1200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ Vue.use(VueSSE);
 const translations = require("./translations/translations.json");
 
 const i18n = new VueI18n({
-  locale: "en", // Use English by default
+  locale: "ru", // Use English by default
   messages: translations
 });
 
